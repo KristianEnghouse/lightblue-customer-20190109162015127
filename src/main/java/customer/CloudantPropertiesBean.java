90708ae1d823ede0b1193c353aa347840bd8d964
Kristian Leiner <kristian.leiner@enghouse.com>
2019-01-09 21:23:31 +0000

    Potential fix when copying take three

--- src/main/java/customer/CloudantPropertiesBean.java
+++ src/main/java/customer/CloudantPropertiesBean.java
@@ -1,63 +1,54 @@
 package customer;
 
-public class Customer {
-    private String _id;
-    private String _rev;
-    private String username;
-    private String password;
-    
-	private String firstName;
-	private String lastName;
-	private String email;
-	private String imageUrl;
-	
-	public String getCustomerId() {
-		return _id;
-	}
-	public void setCustomerId(String customerId) {
-		this._id = customerId;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getImageUrl() {
-		return imageUrl;
-	}
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-	
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+@Component
+@ConfigurationProperties(prefix="spring.cloudant")
+public class CloudantPropertiesBean {
+
+        private String username;
+        private String password;
+        private String host;
+        private String protocol;
+        private int port;
+        private String database;
+
+        public String getUsername() {
+                return username;
+        }
+        public void setUsername(String username) {
+                this.username = username;
+        }
+        public String getPassword() {
+                return password;
+        }
+        public void setPassword(String password) {
+                this.password = password;
+        }
+        public String getHost() {
+                return host;
+        }
+        public void setHost(String host) {
+                this.host = host;
+        }
+        public int getPort() {
+                return port;
+        }
+        public void setPort(int port) {
+                this.port = port;
+        }
+        public String getProtocol() {
+                return protocol;
+        }
+        public void setProtocol(String protocol) {
+                this.protocol = protocol;
+        }
+        public String getDatabase() {
+                return database;
+        }
+        public void setDatabase(String database) {
+                this.database = database;
+        }
 }
+
